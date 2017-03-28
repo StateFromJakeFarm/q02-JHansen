954f4f35b09a06a956f47e9059ba03c3bc9c74cd
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-03-27 22:44:16 -0700

    working on tests

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -23,6 +23,50 @@ TEST(PiezasTest, constructor) {
     }
 }
 
+TEST(PiezasTest, dropPiece) {
+    Piezas myBoard;
+
+    EXPECT_EQ(Blank, myBoard.pieceAt(0, 0));
+
+    myBoard.dropPiece(0);
+
+    EXPECT_EQ(X, myBoard.pieceAt(0, 0));
+
+    myBoard.dropPiece(0);
+
+    EXPECT_EQ(O, myBoard.pieceAt(1, 0));
+    EXPECT_EQ(X, myBoard.pieceAt(0, 0));
+    EXPECT_EQ(Blank, myBoard.pieceAt(2, 0));
+}
+
+TEST(PiezasTest, reset) {
+    Piezas myBoard;
+
+    for(int c=0; c<BOARD_COLS; c++) {
+        for(int r=0; r<BOARD_ROWS; r++)
+            myBoard.dropPiece(c);
+    }
+
+    Piece cur = X;
+    for(int c=0; c<BOARD_COLS; c++) {
+        for(int r=0; r<BOARD_ROWS; r++) {
+            EXPECT_EQ(cur, myBoard.pieceAt(r, c));
+
+            if(cur == X)
+                cur = O;
+            else
+                cur = X;
+        }
+    }
+
+    myBoard.reset();
+
+    for(int c=0; c<BOARD_COLS; c++) {
+        for(int r=0; r<BOARD_ROWS; r++)
+            EXPECT_EQ(Blank, myBoard.pieceAt(r, c));
+    }
+}
+
 TEST(PiezasTest, gameState) {
     Piezas myBoard;
 
