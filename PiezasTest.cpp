bded18d567b61ab4eebb841e9cd787a9e68ae9a2
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-03-27 22:53:42 -0700

    adding tests for gameState()

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,6 +4,8 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
+#include <iostream>
+using namespace std;
  
 class PiezasTest : public ::testing::Test
 {
@@ -16,6 +18,8 @@ class PiezasTest : public ::testing::Test
 
 TEST(PiezasTest, constructor) {
     Piezas myBoard;
+
+    // ensure myBoard initialized with all spaces set to 'Blank'
     for(int r=0; r<BOARD_ROWS; r++) {
         for(int c=0; c<BOARD_COLS; c++) {
             EXPECT_EQ(Blank, myBoard.pieceAt(r, c));
@@ -26,27 +30,36 @@ TEST(PiezasTest, constructor) {
 TEST(PiezasTest, dropPiece) {
     Piezas myBoard;
 
+    // space should be blank to begin with
     EXPECT_EQ(Blank, myBoard.pieceAt(0, 0));
 
+    // place piece
     myBoard.dropPiece(0);
 
+    // ensure piece was placed
     EXPECT_EQ(X, myBoard.pieceAt(0, 0));
 
+    // place another piece in column
     myBoard.dropPiece(0);
 
+    // new piece should be above the old one
     EXPECT_EQ(O, myBoard.pieceAt(1, 0));
+    // old piece should still be intact
     EXPECT_EQ(X, myBoard.pieceAt(0, 0));
+    // space above new piece should still be Blank
     EXPECT_EQ(Blank, myBoard.pieceAt(2, 0));
 }
 
 TEST(PiezasTest, reset) {
     Piezas myBoard;
 
+    // fill myBoard completely
     for(int c=0; c<BOARD_COLS; c++) {
         for(int r=0; r<BOARD_ROWS; r++)
             myBoard.dropPiece(c);
     }
 
+    // ensure myBoard is currently filled
     Piece cur = X;
     for(int c=0; c<BOARD_COLS; c++) {
         for(int r=0; r<BOARD_ROWS; r++) {
@@ -59,8 +72,10 @@ TEST(PiezasTest, reset) {
         }
     }
 
+    // clear myBoard
     myBoard.reset();
 
+    // ensure myBoard was cleared
     for(int c=0; c<BOARD_COLS; c++) {
         for(int r=0; r<BOARD_ROWS; r++)
             EXPECT_EQ(Blank, myBoard.pieceAt(r, c));
@@ -70,14 +85,15 @@ TEST(PiezasTest, reset) {
 TEST(PiezasTest, gameState) {
     Piezas myBoard;
 
+    // X win horizontal
     myBoard.dropPiece(0);
     myBoard.dropPiece(0);
     myBoard.dropPiece(1);
     myBoard.dropPiece(1);
     myBoard.dropPiece(2);
-
     EXPECT_EQ(X, myBoard.gameState());
 
+    // O win vertical
     myBoard.reset();
     myBoard.dropPiece(1);
     myBoard.dropPiece(2);
@@ -85,7 +101,42 @@ TEST(PiezasTest, gameState) {
     myBoard.dropPiece(3);
     myBoard.dropPiece(1);
     myBoard.dropPiece(3);
-
-    myBoard.print();
     EXPECT_EQ(O, myBoard.gameState());
+
+    // tie horizontal
+    myBoard.reset();
+    myBoard.dropPiece(0);
+    myBoard.dropPiece(2);
+    myBoard.dropPiece(1);
+    myBoard.dropPiece(3);
+    EXPECT_EQ(Invalid, myBoard.gameState());
+
+    // tie vertical
+    myBoard.reset();
+    myBoard.dropPiece(1);
+    myBoard.dropPiece(3);
+    myBoard.dropPiece(1);
+    myBoard.dropPiece(3);
+    myBoard.dropPiece(1);
+    myBoard.dropPiece(3);
+    EXPECT_EQ(Invalid, myBoard.gameState());
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
